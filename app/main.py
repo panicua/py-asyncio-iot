4c252ceffb65ac95b2d868c13a4717ce72fd2d00
kekW <panicua12@gmail.com>
2024-06-20 03:00:25 +0300

    add: parallel async device registration

--- app/main.py
+++ app/main.py
@@ -1,27 +1,37 @@
+import asyncio
 import time
+from typing import Any, Awaitable
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
+    # create devices
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+
+    # register devices in parallel
+    start_ = time.perf_counter()
+    hue_light_id, speaker_id, toilet_id = await parallel_handling(
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet)
+    )
+    end_ = time.perf_counter()
+    print("Elapsed for device registration:", end_ - start_)
 
     # create a few programs
     wake_up_program = [
         Message(hue_light_id, MessageType.SWITCH_ON),
         Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(speaker_id, MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up"),
     ]
 
     sleep_program = [
@@ -32,13 +42,17 @@ def main() -> None:
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await service.run_program(wake_up_program)
+    await service.run_program(sleep_program)
+
+
+async def parallel_handling(*services: Awaitable[Any]) -> list:
+    return await asyncio.gather(*services)
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
-    print("Elapsed:", end - start)
+    print("Total elapsed:", end - start)
