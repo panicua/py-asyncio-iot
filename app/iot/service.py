019390db79ef48402ede15684dcb680139eddc6c
kekW <panicua12@gmail.com>
2024-06-25 15:46:46 +0300

    ref: get_device func now async

--- app/iot/service.py
+++ app/iot/service.py
@@ -35,7 +35,7 @@ class IOTService:
         await self.devices[device_id].disconnect()
         del self.devices[device_id]
 
-    def get_device(self, device_id: str) -> Device:
+    async def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
     async def run_program(self, program: list[Message]) -> None:
